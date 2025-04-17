c48d227b6c0b9e4360678abf16ab720bc29059d0
oussama zeraoulia <zeraouliaoussama40@gmail.com>
2025-04-17 20:00:09 +0100

    first commit

--- app/instruments/page.tsx
+++ app/instruments/page.tsx
@@ -1,14 +1,8 @@
 import { createClient } from '@/utils/supabase/server';
 
 export default async function Instruments() {
-
   const supabase = await createClient();
   const { data: instruments } = await supabase.from("instruments").select();
 
-  return (
-  <div>
-    <h1 style={{fontSize:'36px'}}>Hello Oussama</h1>
-    <pre>{JSON.stringify(instruments, null, 2)}</pre>
-  </div>
-  )
+  return <pre>{JSON.stringify(instruments, null, 2)}</pre>
 }
